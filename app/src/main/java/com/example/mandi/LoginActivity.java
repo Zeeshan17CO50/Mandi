a1d68d3870a225b6cffd78980755babff406a0ed
Zeeshan17CO50 <mohammedsiddiquezee@gmail.com>
2020-07-17 17:45:56 +0530

    Login update

--- app/src/main/java/com/example/mandi/LoginActivity.java
+++ app/src/main/java/com/example/mandi/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.mandi;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
--- app/src/main/java/com/example/mandi/MainActivity.java
+++ app/src/main/java/com/example/mandi/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mandi;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
         joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
         loginButton = (Button) findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
